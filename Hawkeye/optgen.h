7084d3a3ef0b345cdaa63d01b4b9655fa8f7f7e4
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2026-09-15 23:46:50 +0530

    Update optgen.h

--- Hawkeye/optgen.h
+++ Hawkeye/optgen.h
@@ -14,15 +14,9 @@ public:
   // history_multiplier: length of tracked history in units of set capacity.
   OPTgen(std::size_t num_sets, std::size_t associativity, std::size_t history_multiplier = 8);
 
-  // Graded signature: processes one access to `address` in set `set_idx`,
-  // returns whether OPT would have hit.
+
   bool access(std::size_t set_idx, uint64_t address);
 
-  // Extended overload used by the ChampSim adapter.
-  //   training_pc  <- the PC of the PREVIOUS access to this address, i.e. the
-  //                   PC whose insertion decision this access retroactively judges.
-  //   train_valid  <- false on a first-ever reference to `address`, in which
-  //                   case there is nothing to judge and NO training must occur.
   bool access(std::size_t set_idx, uint64_t address, uint64_t ip, uint64_t& training_pc, bool& train_valid);
 
 private:
@@ -46,4 +40,4 @@ private:
   bool access_impl(std::size_t set_idx, uint64_t address, uint64_t ip, uint64_t* training_pc, bool* train_valid);
 };
 
-#endif // OPTGEN_H
+#endif
